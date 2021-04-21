942304781e7a3370a45e2f3940fa7526796a876f
Saúl Laínez <saullainez@hotmail.es>
2021-04-21 11:27:46 -0600

    Evento click

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -2,10 +2,18 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 const CounterApp = ({ value }) => {
+
+    //handleAdd
+    const handleAdd = (e) =>{
+        console.log(e);
+    }
+
     return (
         <Fragment>
             <h1>CounterApp</h1>
             <h2> { value } </h2>
+
+            <button onClick={ handleAdd }>+1</button>
         </Fragment>
     );
 }

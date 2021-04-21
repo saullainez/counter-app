5c9599dcb79deb7467d28abcacaeab39568de404
Saúl Laínez <saullainez@hotmail.es>
2021-04-21 08:18:25 -0600

    Componente CounterApp

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import PrimeraApp from './PrimeraApp';
+import CounterApp from './CounterApp'
 
 import './index.css';
 
 const divRoot = document.querySelector('#app');
 
-ReactDOM.render(<PrimeraApp saludo="Hola, soy una prueba" />, divRoot);
+ReactDOM.render(<CounterApp value={ 1 } />, divRoot);

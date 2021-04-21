e576c7159953ade5f05d922d9408a8869853f7be
Saúl Laínez <saullainez@hotmail.es>
2021-04-20 18:51:54 -0600

    Fragment

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const PrimeraApp = () => {
-    return <h1>Hola Mundo</h1>;
+    return (
+        <Fragment>
+            <h1>Hola Mundo</h1>
+            <p>Mi primera aplicación</p>
+        </Fragment>
+    );
 }
 
 export default PrimeraApp;
